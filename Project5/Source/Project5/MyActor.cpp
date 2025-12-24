bc444733a2e850e92ec33a1467eb021f312873c3
IMGROOT32 <henrycho0302@gmail.com>
2025-12-24 18:26:32 +0900

    4

--- Project5/Source/Project5/MyActor.cpp
+++ Project5/Source/Project5/MyActor.cpp
@@ -9,26 +9,50 @@ AMyActor::AMyActor()
 }
 
 // Called when the game starts or when spawned
-void AMyActor::BeginPlay()
+void AMyActor::Turn()
 {
-	Super::BeginPlay();
-	FVector ActorLocation = GetActorLocation();
-	
+	int ranX = FMath::RandRange(0, 360);
+	int ranY = FMath::RandRange(0, 360);
+	int ranZ = FMath::RandRange(0, 360);
+	FRotator rotation(ranX, ranY, ranZ);
+	SetActorRotation(rotation);
+		
+}
+void AMyActor::Move()
+{
+	int ranX = FMath::RandRange(0, 500);
+	int ranY = FMath::RandRange(0, 500);
+	int ranZ = FMath::RandRange(0, 20);
+	FVector location(ranX, ranY, ranZ);
+	SetActorLocation(location);
 	if (GEngine)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, TEXT("Hello Unreal5"));
-		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Blue, FString::Printf(TEXT("Actor : %s"), *GetName()));
-		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Blue, FString::Printf(TEXT("Actor : %s"), *ActorLocation.ToString()));
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Blue, FString::Printf(TEXT("Rotation : %s"), *location.ToString()));
 	}
-
-	SetActorLocation(FVector(0, 0, 0));
-	SetActorRotation(FRotator(100.f, 100.f, 100.f));
 }
 
+	void AMyActor::BeginPlay()
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Blue, TEXT("HI"));
+		}
+
+		SetActorLocation(FVector(0, 50, 0));
+		ElapsedTime = 0.0f;
+		currentCount = 0;
+	}
+
+
 // Called every frame
-void AMyActor::Tick(float DeltaTime)
+	void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	if (ElapsedTime >= 1.0f)
+	{
+		Move();
+		Turn();
+		ElapsedTime = 0.0f;
+	}
 }
 
--- Project5/Source/Project5/MyActor.h
+++ Project5/Source/Project5/MyActor.h
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
-#include <CoreMinimal.h>
-
+#include "Engine/Engine.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
 #include "MyActor.generated.h"
 
 UCLASS()
@@ -19,6 +19,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+	void Turn();
+	void Move();
+	float ElapsedTime = 0.0f;
+	int currentCount;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
